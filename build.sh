51c9b6cc02a17df9ca3e80ddb4ac3a3db956b245
Jonatas <j.teodoro@me.com>
2026-03-07 02:43:02 -0300

    Forçando upgrade do pip e instalacao limpa do django

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrategit add build.sh
