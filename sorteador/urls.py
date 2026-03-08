9482750253f9dd0c7a6d7486369db6b7a70246ee
Jonatas <j.teodoro@me.com>
2026-03-08 17:04:47 -0300

    Corrigindo URLs e garantindo Django no requirements

--- sorteador/urls.py
+++ sorteador/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Define as rotas específicas do aplicativo de sorteio
 urlpatterns = [
+    # Liga a URL vazia à função que executa o sorteio na tela
     path('', views.sorteador_view, name='sorteador'),
 ]
--- sorteador_jonatas/urls.py
+++ sorteador_jonatas/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Define as rotas principais do sistema
 urlpatterns = [
+    # Rota para a interface de administração do Django
     path('admin/', admin.site.urls),
-    path('', include('sorteador_jonatas.urls')), 
+    
+    # Encaminha o acesso da página inicial para o aplicativo 'sorteador'
+    path('', include('sorteador.urls')),
 ]
